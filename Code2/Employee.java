ffad1e517c9bd0a5a411f00d70cc24154cd1991f
amitsrivastava4all <amitsrivastava4all@gmail.com>
2018-12-28 17:51:09 +0530

    Code

---
+++ Code2/Employee.java
@@ -0,0 +1,27 @@
+//class Person{
+//	int age;
+//}
+public class Employee  {
+	int id ; // Member Variables of a class
+	String name;
+	double salary;
+	
+	void print() {
+		System.out.println("Id "+this.id);
+		System.out.println("name "+name);
+		System.out.println("Salary "+salary);
+	}
+
+//	// Logic for Asking Account Number
+//			// Verify Account Number
+//			// Check Balance
+//	void withDraw() {
+//		
+//		// Deduct Balance
+//	}
+	
+	
+
+}
+
+
---
+++ Code2/TestEmployee.java
@@ -0,0 +1,21 @@
+
+public class TestEmployee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee ram = new Employee();
+		ram.print();
+//		System.out.println("Id "+ram.id);
+//		System.out.println("name "+ram.name);
+//		System.out.println("Salary "+ram.salary);
+		int x = 10;
+		System.out.println("***************************");
+		Employee shyam = new Employee();
+		shyam.print();
+//		System.out.println(shyam.id);
+//		System.out.println(shyam.name);
+//		System.out.println(shyam.salary);
+
+	}
+
+}
